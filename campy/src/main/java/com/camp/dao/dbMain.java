94a3384f51a1c95d47443a5f4eb214ffd1c5acb9
kimsia0717 <https://Kimsia0717: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@github.com/Kimsia0717/teamC_campYo>
2022-01-20 11:17:22 +0900

    회원 탈퇴 마이페이지 정보 불러오기 추가

--- campy/src/main/java/com/camp/dao/dbMain.java
+++ campy/src/main/java/com/camp/dao/dbMain.java
@@ -9,14 +9,20 @@ public class dbMain {
         memberDTO dto = new memberDTO();
         memberDAO dao = new memberDAO();
 
-        dto.setUser_id("bong123");
-        dto.setUser_pass("@1234567");
-        dto.setUser_name("리봉철");
-        dto.setUser_tel("01015354685");
-        dto.setUser_email("bong0717@naver.com");
+//        dto.setUser_id("bong123");
+//        dto.setUser_pass("@1234567");
+//        dto.setUser_name("리봉철");
+//        dto.setUser_tel("01015354685");
+//        dto.setUser_email("bong0717@naver.com");
         // dao.(dto);
         // int result = dao.getLoginResult(dto);
         // System.out.println(result);
+        String email = "kimsia0717@naver.com";
+        int atidx = email.indexOf("@");
+        String result1 = dao.subStr(email, 0, atidx);
+        String result2 = dao.subStr(email, atidx + 1);
+        System.out.println(result1);
+        System.out.println(result2);
     }
 
 }
