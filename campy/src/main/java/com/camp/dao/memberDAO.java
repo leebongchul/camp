ca98ea1dd6aadc05924aff55f7bb9c89733d34e9
kimsia0717 <https://Kimsia0717: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@github.com/Kimsia0717/teamC_campYo>
2022-01-20 16:45:38 +0900

    비밀번호 변경 추가

--- campy/src/main/java/com/camp/dao/memberDAO.java
+++ campy/src/main/java/com/camp/dao/memberDAO.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import com.camp.dto.memberDTO;
 
 public class memberDAO extends DBconn {
-    //
+    // 회원가입
     public int insert(memberDTO dto) {
         int result = 0;
         String sql = "insert into member values(?,?,?,?,?,sysdate,?)";
@@ -81,6 +81,7 @@ public class memberDAO extends DBconn {
 
     }
 
+    // 중복찾기
     public ArrayList<memberDTO> idCheck() {
         ArrayList<memberDTO> list = new ArrayList<memberDTO>();
 
@@ -107,7 +108,7 @@ public class memberDAO extends DBconn {
     // 비밀번호 수정전 정보 체크
     public int getPassCheck(memberDTO dto) {
         int result = 0;
-        String sql = "select count(*) from member where user_id = ? and user_name = ? and user_tel = ? and user_email = ?";
+        String sql = "select count(*) from member where user_id = ? and user_name = ? and user_tel = ? and user_email = ? and user_exityn = 'y'";
         getPreparedStatement(sql);
 
         String tel = dto.getHp1() + dto.getHp2() + dto.getHp3();
@@ -124,6 +125,21 @@ public class memberDAO extends DBconn {
             while (rs.next()) {
                 result = rs.getInt(1);
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    // 비밀번호변경
+    public int PassUpdate(memberDTO dto) {
+        int result = 0;
+        String sql = "update member set user_pass = ? where user_id = ? ";
+        getPreparedStatement(sql);
+        try {
+            pstmt.setString(1, dto.getUser_pass());
+            pstmt.setString(2, dto.getUser_id());
+            result = pstmt.executeUpdate();
         } catch (Exception e) {
             e.printStackTrace();
         }
